4bd0a6431d1cde4c400ee7a523e1269b8e21e391
mirlz <mirlzy@gmail.com>
2022-07-01 01:38:00 +0800

    test associate user collection with blogPost and comment collection
    blogpost added in user should point to the record in blogpost collection

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -6,7 +7,7 @@ const BlogPost = require('../src/blogPost');
 describe('associations', () => {
     let joe, blogPost, comment; 
 
-    beforeEach(() => {
+    beforeEach((done) => {
         joe = new User({ name: 'Joe'});
         blogPost = new BlogPost({ title: 'JS is great', content: 'Uh huh'});
         comment = new Comment({ content: 'weeeeeee' });
@@ -14,5 +15,22 @@ describe('associations', () => {
         joe.blogPosts.push(blogPost);
         blogPost.comments.push(comment);
         comment.user = joe;
+
+        Promise.all([
+            joe.save(),
+            blogPost.save(),
+            comment.save()
+        ]).then(() => {
+            done();
+        })
     });
+
+    it('saves a relation between a user and a blogpost', (done) => {
+        User.findOne({ name: 'Joe' })
+            .populate('blogPosts')
+            .then((user) => {
+                assert(user.blogPosts[0].title === 'JS is great');
+                done();
+            })
+    })
 });
